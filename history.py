af784b0ba7df8ca45366d3b2510240abd56a9b24
mrjohnnycake <103395689+mrjohnnycake@users.noreply.github.com>
2025-04-22 13:03:46 -0700

    Update history.py
    change firefox to floorp

--- history.py
+++ history.py
@@ -4,37 +4,37 @@ import shutil
 import configparser
 import os
 
-class FirefoxHistory():
-    def __init__(self, firefox_path: str):
+class FloorpHistory():
+    def __init__(self, floorp_path: str):
         #   Results number
         self.limit = None
 
         #   Set history location
-        history_location = self.searchPlaces(firefox_path)
+        history_location = self.searchPlaces(floorp_path)
 
         #   Temporary  file
-        #   Using FF63 the DB was locked for exclusive use of FF
+        #   Using FF63 the DB was locked for exclusive use of Floorp
         #   TODO:   Regular updates of the temporary file
         temporary_history_location = tempfile.mktemp()
         shutil.copyfile(history_location, temporary_history_location)
-        #   Open Firefox history database
+        #   Open Floorp history database
         self.conn = sqlite3.connect(temporary_history_location)
         #   External functions
         self.conn.create_function('hostname',1,self.__getHostname)
 
-    def searchPlaces(self, firefox_path: str):
-        #   Firefox folder path
-        firefox_path = os.path.expanduser(firefox_path)
-        if not firefox_path.endswith("/"):
-            firefox_path += "/"
-        #   Firefox profiles configuration file path
-        conf_path = os.path.join(firefox_path,'profiles.ini')
+    def searchPlaces(self, floorp_path: str):
+        #   Floorp folder path
+        floorp_path = os.path.expanduser(floorp_path)
+        if not floorp_path.endswith("/"):
+            floorp_path += "/"
+        #   Floorp profiles configuration file path
+        conf_path = os.path.join(floorp_path,'profiles.ini')
         #   Profile config parse
         profile = configparser.RawConfigParser()
         profile.read(conf_path)
         prof_path = profile.get("Profile0", "Path")
         #   Sqlite db directory path
-        sql_path = os.path.join(firefox_path,prof_path)
+        sql_path = os.path.join(floorp_path,prof_path)
         #   Sqlite db path
         return os.path.join(sql_path,'places.sqlite')
 
